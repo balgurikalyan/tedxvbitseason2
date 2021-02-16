d4acb3135214ccdf605be53358ad8618bfbc35f6
kalyan rao <balgurikalyan711.com>
2021-02-16 20:58:33 +0530

    files uploaded

---
+++ README.txt
@@ -0,0 +1,3 @@
+Webisite for TEDxVBIT's Season 2.
+
+This site contains all of TEDxVBIT's Season 2. Browse for everything you need to know about this season.
---
+++ mail.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<?php
+mail('you@yourmail.com','Test mail','The mail function is working!');
+echo 'Mail sent!';
+?>
+
+</body>
+</html>
+
+
